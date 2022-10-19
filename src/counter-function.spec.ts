872b41d24b4347ed582ed06fe16a6329f010b477
brianmuturi2 <brianmuturi2@gmail.com>
2022-10-19 20:36:16 +0300

    question(2): updated question 2 and test

--- src/counter-function.spec.ts
+++ src/counter-function.spec.ts
@@ -1,17 +1,21 @@
-import {counter} from './counter-function';
+import {counter, returnNumber, returnVoid} from './counter-function';
 
 describe("counter function", function() {
-    const [getA, nextA] = counter()
+    let getA: returnNumber;
+    let nextA: returnVoid;
+    beforeEach(() => {
+        [getA, nextA] = counter()
+    })
     it("has initial value of zero", function() {
-          const initialVal = getA();
-          expect(initialVal).toBe(0);
+          expect(getA()).toBe(0);
     });
-    it("can increment initial value", function() {
+    it("can increment value by 1", function() {
         nextA();
         expect(getA()).toBe(1);
     });
     it("can create independent instances of counter", function() {
         const [getB, nextB] = counter(10);
+        nextA();
         expect(getA()).toBe(1)
         nextB();
         expect(getB()).toBe(11)
--- src/counter-function.ts
+++ src/counter-function.ts
@@ -1,7 +1,7 @@
-type returnNumber = (() => number);
-type returnVoid = (() => void);
+export type returnNumber = (() => number);
+export type returnVoid = (() => void);
 
-export function counter (initial: number = 0): Array<returnNumber|returnVoid> {
+export function counter (initial: number = 0): [returnNumber, returnVoid] {
     const functionOne = (): number => {
         return initial;
     };
