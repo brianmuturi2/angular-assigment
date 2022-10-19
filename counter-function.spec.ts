12c75bde652ef7c5be835da5de5a1ad611ce6085
Brian Muturi <brianmuturi2@gmail.com>
2022-10-19 11:13:59 +0300

    Create counter-function.spec.ts

---
+++ counter-function.spec.ts
@@ -0,0 +1,17 @@
+describe("counter function", function() {
+    const [getA, nextA] = counter()
+    it("has initial value of zero", function() {
+          const initialVal = getA();
+          expect(initialVal).toBe(0);
+    });
+    it("can increment initial value", function() {
+        nextA();
+        expect(getA()).toBe(1);
+    });
+    it("can create independent instances of counter", function() {
+        const [getB, nextB] = counter(10);
+        expect(getA()).toBe(1)
+        nextB();
+        expect(getB()).toBe(11)
+    });
+});
